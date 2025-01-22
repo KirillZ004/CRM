37aee5070e67dd71310110232ca788d6c01278d6
Your Name <you@example.com>
2025-01-22 13:22:46 +0700

    запросы

--- api/auth/AuthCheck.php
+++ api/auth/AuthCheck.php
@@ -1,38 +1,31 @@
-<?php
-
-
-$_SESSION['token'] = '';
-
-function AuthCheck($successPath = '', $errorPath = '') {
-    require_once 'api/DB.php';
-
-    if (!isset($_SESSION['token']) && $errorPath) {  
-        header("Location: " . $errorPath);  
-        return;  
-    }
-
-
-    //токен текущего пользователя
-    $token = $_SESSION['token'];
-    $adminID = $DB->query(
-        "SELECT id FROM users WHERE token = '$token'
-        ")->fetchAll();
-
-
-
-     
-    //Если adminId пустой - редирект на $errorPath 
-    if (empty($adminID) && $errorPath) { 
-        header("Location: " . $errorPath); 
-       
-    } 
-    //Если adminId не пустой - редирект на $successPath 
-    if (!empty($adminID) && $successPath) { 
-        header("Location: " . $successPath); 
-       
-    }  
+<?php 
  
-}
+//$_SESSION['token'] = '';  
 
+ 
+function AuthCheck($successPath = '', $errorPath = '') {
+    require_once 'api/DB.php'; 
+    if (!isset($_SESSION['token']) && $errorPath) { 
+        header("Location: " . $errorPath); 
+        return; 
+    }
+    //токен текущего пользователя 
+    $token = $_SESSION['token']; 
+    $adminID = $DB->query( 
+        "SELECT id FROM users WHERE token = '$token' 
+        ")->fetchAll();   
+    
+    //Если adminId пустой - редирект на $errorPath
+    if (empty($adminID) && $errorPath) {
+        header("Location: " . $errorPath);
 
+    }
+    //Если adminId не пустой - редирект на $successPath
+    if (!empty($adminID) && $successPath) {
+        header("Location: " . $successPath);
+
+    }
+} 
+ 
+ 
 ?>
--- login.php
+++ login.php
@@ -1,11 +1,14 @@
 <?php
-
 session_start();
 
 require_once 'api/auth/AuthCheck.php';
-AuthCheck('clients.php');
 
+AuthCheck('clients.php');
+//Сделать:
+//Вывод ошибки для пароля
+//Покрасить ошибку в красный цвет и сделать
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,21 +16,40 @@ AuthCheck('clients.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRM | Авторизация</title>
-    <link rel="stylesheet" href="styles/settings.css"> 
+    <link rel="stylesheet" href="styles/settings.css">
     <link rel="stylesheet" href="styles/pages/login.css">
 </head>
 <body>
-    <div class="login-container">
-        <!-- Форма с полями логин, пароль и кнопкой войти -->
-        <form class="login-form" action="api/auth/login.php" method="post">
-            <label for="username">Логин:</label>
-            <input type="text" id="username" name="username" required>
-            
-            <label for="password">Пароль:</label>
-            <input type="password" id="password" name="password" required>
-            
-            <button type="submit">Войти</button>
-        </form>
-    </div>
+    <header>
+        <div class="container">
+            <form action="api/auth/AuthUser.php" method="POST" class="login-form">
+                <div class="form-group">
+                    <label for="username">Логин</label>
+                    <input type="text" id="username" name="username" >
+                    <p class="error" style = "Color:red;">
+                        <?php
+                        if (isset($_SESSION['login-errors'])){
+                            $errors = $_SESSION['login-errors'];
+                            echo isset($errors['login']) ? $errors['login'] : '';
+                        }
+                        ?>
+                    </p>
+                </div>
+                <div class="form-group">
+                    <label for="password">Пароль</label>
+                    <input type="password" id="password" name="password" >
+                    <p class="error" style = "Color:red;">
+                        <?php
+                        if (isset($_SESSION['password-errors'])){
+                            $errors = $_SESSION['password-errors'];
+                            echo isset($errors['password']) ? $errors['password'] : '';
+                        }
+                        ?>
+                    </p>
+                </div>
+                <button type="submit" class="login-button">Войти</button>
+            </form>
+        </div>
+    </header>
 </body>
 </html>
